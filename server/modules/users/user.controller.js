115448f4fc5b26b4750ca34b7f62a60a20f45ce2
Tran Tan Loc <trantanloc3689@gmail.com>
2019-05-18 23:11:26 +0700

    add token and bcrypt

--- server/modules/users/user.controller.js
+++ server/modules/users/user.controller.js
@@ -3,8 +3,34 @@ const CONSTANTS = require("../../config/constants");
 const bcrypt = require("bcryptjs");
 
 module.exports = {
-    login: (req, res)=>{
-        res.json("dang nhap");
+    login: async (req, res)=>{
+        let user = await User.find({username: req.body.username});
+        if(user.length != 0){
+            let isValid = bcrypt.compareSync(req.body.password, user[0].password);
+            if( isValid == true){
+                let token = jwt.sign({ data: user[0] }, CONSTANTS.SECRET_KEY, { expiresIn: 3600*60});
+                let result = {
+                    "message" : "success",
+                    "data" : user[0],
+                    "token": token
+                };
+                res.json(result);
+            } else{
+                let result = {
+                    "message" : "password is not valid",
+                    "data" : "",
+                    "token": ""
+                };
+                res.json(result);
+            }
+        }else{
+            result = {
+                "message" : "username not found",
+                "data" : "",
+                "token": ""
+            };
+            res.json(result);
+        }
     },
     register: async (req, res)=>{
         let newUser = await User.create({
