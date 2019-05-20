7b82c012426191ef970740ee80c3d96fc4ac7175
NgoMinhNhi <ngominhnhi1998@gmail.com>
2019-05-20 14:12:17 +0700

    'clientSide'

---
+++ client/src/components/Header.vue
@@ -0,0 +1,142 @@
+
+<template>
+    <div id="header">
+            <div class="logo">
+                <a href="index.html">
+                    <span class="fa fa-home marker"></span>
+                    <span class="logoText">reales</span>
+                </a>
+            </div>
+            <a href="#" class="navHandler"><span class="fa fa-bars"></span></a>
+            <div class="search">
+                <span class="searchIcon icon-magnifier"></span>
+                <input type="text" placeholder="Search for houses, apartments...">
+            </div>
+            <div class="headerUserWraper">
+                <a href="#" class="userHandler dropdown-toggle" data-toggle="dropdown"><span class="icon-user"></span><span class="counter">5</span></a>
+                <a href="#" class="headerUser dropdown-toggle" data-toggle="dropdown">
+                    <img class="avatar headerAvatar pull-left" src="images/avatar-1.png" alt="avatar">
+                    <div class="userTop pull-left">
+                        <span class="headerUserName">John Smith</span> <span class="fa fa-angle-down"></span>
+                    </div>
+                    <div class="clearfix"></div>
+                </a>
+                <div class="dropdown-menu pull-right userMenu" role="menu">
+                    <div class="mobAvatar">
+                        <img class="avatar mobAvatarImg" src="images/avatar-1.png" alt="avatar">
+                        <div class="mobAvatarName">John Smith</div>
+                    </div>
+                    <ul>
+                        <li><a href="#"><span class="icon-settings"></span>Settings</a></li>
+                        <li><a href="profile.html"><span class="icon-user"></span>Profile</a></li>
+                        <li><a href="#"><span class="icon-bell"></span>Notifications <span class="badge pull-right bg-red">5</span></a></li>
+                        <li class="divider"></li>
+                        <li><a href="#"><span class="icon-power"></span>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="headerNotifyWraper">
+                <a href="#" class="headerNotify dropdown-toggle" data-toggle="dropdown">
+                    <span class="notifyIcon icon-bell"></span>
+                    <span class="counter">5</span>
+                </a>
+                <div class="dropdown-menu pull-right notifyMenu" role="menu">
+                    <div class="notifyHeader">
+                        <span>Notifications</span>
+                        <a href="#" class="notifySettings icon-settings"></a>
+                        <div class="clearfix"></div>
+                    </div>
+                    <ul class="notifyList">
+                        <li>
+                            <a href="#">
+                                <img class="avatar pull-left" src="images/avatar-1.png" alt="avatar">
+                                <div class="pulse border-grey"></div>
+                                <div class="notify pull-left">
+                                    <div class="notifyName">Sed ut perspiciatis unde</div>
+                                    <div class="notifyTime">5 minutes ago</div>
+                                </div>
+                                <div class="clearfix"></div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="notifyRound notifyRound-red fa fa-envelope-o"></div>
+                                <div class="pulse border-red"></div>
+                                <div class="notify pull-left">
+                                    <div class="notifyName">Lorem Ipsum is simply dummy text</div>
+                                    <div class="notifyTime">20 minutes ago</div>
+                                </div>
+                                <div class="clearfix"></div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="notifyRound notifyRound-yellow fa fa-heart-o"></div>
+                                <div class="pulse border-yellow"></div>
+                                <div class="notify pull-left">
+                                    <div class="notifyName">It is a long established fact</div>
+                                    <div class="notifyTime">2 hours ago</div>
+                                </div>
+                                <div class="clearfix"></div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="notifyRound notifyRound-magenta fa fa-paper-plane-o"></div>
+                                <div class="pulse border-magenta"></div>
+                                <div class="notify pull-left">
+                                    <div class="notifyName">There are many variations</div>
+                                    <div class="notifyTime">1 day ago</div>
+                                </div>
+                                <div class="clearfix"></div>
+                            </a>
+                        </li>
+                    </ul>
+                    <a href="#" class="notifyAll">See All</a>
+                </div>
+            </div>
+            <a href="#" class="mapHandler"><span class="icon-map"></span></a>
+            <div class="clearfix"></div>
+        </div>
+</template>
+
+<script>
+import '../assets/stylesheets/font-awesome.css'
+import '../assets/stylesheets/simple-line-icons.css'
+import '../assets/stylesheets/jquery-ui.css'
+import '../assets/stylesheets/datepicker.css'
+import '../assets/stylesheets/bootstrap.css'
+import '../assets/stylesheets/app.css'
+
+export default {
+    name: 'headerSide',
+//     mounted() {
+//     const  str= ['assets/scripts/jquery-2.1.1.min.js',
+//                 'assets/scripts/jquery-ui.min.js',
+//                 'assets/scripts/jquery-ui-touch-punch.js',
+//                 'assets/scripts/jquery.placeholder.js',
+//                 'assets/scripts/bootstrap.js',
+//                 'assets/scripts/jquery.touchSwipe.min.js',
+//                 'assets/scripts/jquery.slimscroll.min.js',
+//                 'http://maps.googleapis.com/maps/api/js?sensor=true&amp;libraries=geometry&amp;libraries=places',
+//                 'assets/scripts/infobox.js',
+//                 'assets/scripts/jquery.tagsinput.min.js',
+//                 'assets/scripts/bootstrap-datepicker.js',
+//                 'assets/scripts/app.js'];
+//     for(var i =0;i<str.length;i++) {
+//         const plugin = document.createElement("script");
+//         plugin.setAttribute(
+//         "src",
+//         str[i]
+//         );
+//         plugin.async = true;
+//         document.head.appendChild(plugin);
+//     }
+//   },
+}
+</script>
+
+<style>
+
+</style>
+
