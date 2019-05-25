198a7a01eb9c27ce858b72b455557ab7be4af8f4
duyquoc1508 <duyquoc.nguyen.bk@gmail.com>
2019-05-25 10:31:56 +0700

    add new feature/approve_post and fix feature/update_post in modules classified

--- server/modules/classified/classified.controller.js
+++ server/modules/classified/classified.controller.js
@@ -1,30 +1,45 @@
+const unit_price = [0,10000,20000,30000];
+function calculateBalance(current_balance,time_post, level_post = 1){
+   let account_blance = current_balance - time_post * unit_price[level_post];
+   return account_blance;
+};
+
 module.exports = {
-   createPost : async (req,res,next) =>{
+   createPost : async (req,res) =>{
       try{
          let list_img_url = [];
          let length = req.files.length|0;
          for(i = 0; i < length; i++){
             list_img_url.push(req.files[i].filename);
          };
-         let newClassified = await Classified.create({
+         
+         let newClassified = {
             id_user: req.user.data._id,
             title: req.body.title,
             price: req.body.price,
             area : req.body.area,
+            level: req.body.level,
             content: req.body.content,
             category: req.body.category,
             requirement : req.body.requirement,
             address: req.body.address,
             images : list_img_url,
             time_post: req.body.time_post,
-         });
+         };
 
-         // //add id classified into table User
+         let user_create = await User.findById(newClassified.id_user);
+         let account_balance = calculateBalance(user_create.amount, newClassified.time_post, newClassified.level);
+         if(account_balance < 0){
+            throw new Error("Your account balance not enough! Please pay into your account or reduce post time");
+         }
+
+         let newClassiiedCreated = await Classified.create(newClassified);
+         //add id classified into table User
          await User.findOneAndUpdate({_id: newClassified.id_user},
-                     {$push: {id_classified: newClassified._id}});
+                     {$push: {id_classified: newClassiiedCreated._id}});
 
          var result = {
-            "data": newClassified,
+            "data": newClassiiedCreated,
             "message": "Create successfully",
             "status": true
          };
@@ -39,7 +54,7 @@ module.exports = {
       res.json(result);
    },
 
-   updatePost : async(req, res, next) =>{
+   updatePost : async(req, res) =>{
       try {
          let objectClassifiedUpdate = await Classified.findById(req.params.id);
 
@@ -48,29 +63,64 @@ module.exports = {
             throw new Error('Action denied! You are not author of the post');
          }
 
-         else{
-            let list_img_url = [];
-            let length = req.files.length|0;
-            for(i = 0; i < length; i++){
-            list_img_url.push(req.files[i].filename);
+         let list_img_url = [];
+         let length = req.files.length|0;
+         for(i = 0; i < length; i++){
+         list_img_url.push(req.files[i].filename);
+         };
+         var classifiedUpdate = {
+            status: 0,
+            level: req.body.level,
+            title: req.body.title,
+            price: req.body.price,
+            area : req.body.area,
+            content: req.body.content,
+            category: req.body.category,
+            requirement : req.body.requirement,
+            address: req.body.address,
+            images : list_img_url,
+            time_post: req.body.time_post,
+            last_modified: Date.now()
+         };
+
+         let user_update = await User.findById(objectClassifiedUpdate.id_user).select("amount transaction");
+         let amount_before_update = current_balance = user_update.amount;
+
+         if(objectClassifiedUpdate.status == 1){
+            let one_day = 24*60*60*1000;
+            let diff_days = Math.round((Date.now() - objectClassifiedUpdate.time_approved)/one_day);
+            var account_balance = unit_price[objectClassifiedUpdate.level] * (objectClassifiedUpdate.time_post - diff_days - 1);
+            current_balance += account_balance;
+
+            let account_balance_available = calculateBalance(current_balance,classifiedUpdate.time_post, classifiedUpdate.level);
+            if(account_balance_available < 0){
+               throw new Error("Your account balance not enough! Please pay into your account or reduce post time or reduce level");
             };
-            var classifiedUpdate = {
-               title: req.body.title,
-               price: req.body.price,
-               area : req.body.area,
-               content: req.body.content,
-               category: req.body.category,
-               requirement : req.body.requirement,
-               address: req.body.address,
-               images : list_img_url,
-               time_post: req.body.time_post,
-               last_modified: Date.now()
+
+            await Classified.findOneAndUpdate({_id:req.params.id}, classifiedUpdate);
+
+            let transaction = {
+               transaction_amount: account_balance,
+               transaction_content: "Refund the transaction amount"
+            }
+            await User.findOneAndUpdate(
+               { _id: objectClassifiedUpdate.id_user },
+               { amount: current_balance, $push: {transaction: transaction}},
+            );
+         }
+
+         else{
+            let account_balance = calculateBalance(amount_before_update,classifiedUpdate.time_post, classifiedUpdate.level);
+            if(account_balance < 0){
+               throw new Error("Your account balance not enough! Please pay into your account or reduce post time");
             };
             
             await Classified.findOneAndUpdate({_id:req.params.id}, classifiedUpdate);
          }
          var result ={
-            "data": classifiedUpdate,
+            "amount_before_update": amount_before_update,
+            "amount_after_update": current_balance,
+            "data_update": classifiedUpdate,
             "status": true,
             "message": "Updated successfully"
          };
@@ -84,10 +134,9 @@ module.exports = {
       res.json(result);
    },
 
-   deletePost : async (req, res, next) =>{
+   deletePost : async (req, res) =>{
       try {
          let objectClassifiedDelete = await Classified.findById(req.params.id);
-         //findById(): if not found => error
 
          // check author of post
          if(req.user.data._id != objectClassifiedDelete.id_user){
@@ -97,7 +146,7 @@ module.exports = {
          await Classified.findOneAndDelete({_id:req.params.id});
 
          //delete id_classified in table user
-         await User.update(
+         await User.findOneAndUpdate(
             { _id:req.user.data._id },
             { $pull: {id_classified: req.params.id }
          });
@@ -116,33 +165,37 @@ module.exports = {
       res.json(result);
    },
 
-   updateLevelPost: async(req,res) => {
-      try{
-         let postUpdateLevel = await Classified.findById(req.params.id);
-         if(req.user.data._id != postUpdateLevel.id_user){
-            throw new Error("Action denied! You are not author of the post");
-         }
-
-         // if(postUpdateLevel.status == 0){
-         //    throw new Error("Post is pendding! Can't upgrade level");
-         // }
-
-         await Classified.findOneAndUpdate(
+   //duyệt bài price: 10k once day
+   aprrovePost: async (req,res) =>{
+      try {
+         let classified_approved = await Classified.findOneAndUpdate(
             { _id: req.params.id },
-            { level: req.body.level }
+            { status: 1, time_approved: Date.now() }
          );
-         var result = {
-            "status": true,
-            "message": "Update level successfully!"
+         let transaction_value = 0 - classified_approved.time_post * unit_price[classified_approved.level];
+         let transaction = {
+            transaction_amount: transaction_value,
+            transaction_content: "Pay for post",
          }
-      }
-      catch(error){
-         var result ={
+         let user = await User.findOne({_id: classified_approved.id_user});
+         await User.findOneAndUpdate(
+            {_id: classified_approved.id_user},
+            { $push: {transaction: transaction}, amount: user.amount + transaction_value }
+         );
+
+         var result = {
+            "transaction_info" : transaction,
+            "status": true,
+            "message": "Post is approved!"
+         }
+      } catch (error) {
+         var result = {
             "status": false,
-            "message": "Update false",
             "error_msg": error.message
          }
       }
       res.json(result);
-   }
+   },
+
+
 };
