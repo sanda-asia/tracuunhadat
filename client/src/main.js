f098234374e1f1389ec9e6b793fae083d5e15f7d
NgoMinhNhi <ngominhnhi1998@gmail.com>
2019-06-05 12:40:15 +0700

    them tinh nang login

--- client/src/main.js
+++ client/src/main.js
@@ -3,11 +3,16 @@
 /* eslint-disable import/first */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from 'bootstrap-vue';
+
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 import Resource from 'vue-resource';
 import Vuetify from 'vuetify'
+Vue.use(BootstrapVue);
 
 Vue.use(Vuetify)
 
@@ -29,6 +34,9 @@ Vue.component('mini-poster', MiniPoster);
 import ModalPopUp from './layouts/ModalPopUp';
 Vue.component('modal-pop-up', ModalPopUp);
 
+import LoginAndRegister from './layouts/LoginAndRegister';
+Vue.component('login-and-register', LoginAndRegister);
+
 import Pagination from './components/Pagination';
 Vue.component('pagination', Pagination);
 
@@ -37,14 +45,6 @@ Vue.component('left-nav', leftNav);
 
 import LoginModal from './components/LoginModal';
 Vue.component('login-modal', LoginModal);
-
-import jQuery from 'jquery';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import BootstrapVue from 'bootstrap-vue';
-Vue.use(BootstrapVue);
-
-global.jQuery = jQuery;
-
 Vue.use(Resource);
 
 /* eslint-disable no-new */
